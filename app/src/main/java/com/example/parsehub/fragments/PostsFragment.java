5860bf5e723f8b142e0b16ed7e62e76b39683bc2
Brandon <brandonkieng182@gmail.com>
2022-03-14 18:05:27 -0700

    refresh and scrolling

--- app/src/main/java/com/example/parsehub/fragments/PostsFragment.java
+++ app/src/main/java/com/example/parsehub/fragments/PostsFragment.java
@@ -7,12 +7,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.parsehub.EndlessRecyclerViewScrollListener;
 import com.example.parsehub.Post;
 import com.example.parsehub.PostsAdapter;
 import com.example.parsehub.R;
@@ -30,6 +32,8 @@ public class PostsFragment extends Fragment {
     private RecyclerView rvPosts;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
+    SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -56,8 +60,34 @@ public class PostsFragment extends Fragment {
         //set the adapter on the recycler view
         rvPosts.setAdapter(adapter);
         //set the layout manager on the recycler view
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(manager);
         queryPost();
+
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        /*
+        swipeContainer.setColorSchemeColors(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+                */
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("newdata","getting data");
+                queryPost();
+            }
+        });
+
+        scrollListener = new EndlessRecyclerViewScrollListener(manager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.i("infinite" ,"loading more" + page);
+                queryPost();
+            }
+        };
+        //adds scroll Listener to RecyclerView
+        rvPosts.addOnScrollListener(scrollListener);
     }
 
     protected void queryPost() {
